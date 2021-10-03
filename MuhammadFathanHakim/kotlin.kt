f21e2dcb9db26f396d52df5b8267735605dc115e
Fathkim <88641596+Fathkim@users.noreply.github.com>
2021-10-03 21:25:27 +0700

    kotlin.kt

---
+++ MuhammadFathanHakim/kotlin.kt
@@ -0,0 +1,17 @@
+// mapKeys
+
+fun main() {
+   val nama = mapOf(
+       "Fathan" to "Ganteng",
+       "Fadi" to "Normal",
+       "Ka Farros" to "1 JT IQ"
+   )
+
+   val mapKeys = nama.keys
+   val mapValue = nama.values
+
+   println(mapKeys)
+   println(mapValue)
+}
+
+
